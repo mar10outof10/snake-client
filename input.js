8c120447610d95aedc7a29b13b96b0037dd0b3c1
Martin Halas <echoes.time@gmail.com>
2021-03-10 01:15:49 +0000

    Added witty and fresh messages to send from snek

--- input.js
+++ input.js
@@ -25,6 +25,12 @@ const handleUserInput = connection => {
       connection.write('Move: left');
     } else if (key === 'd') {
       connection.write('Move: right');
+    } else if (key === 'p') {
+      connection.write('Say: ahhhhhhhhhhhhhh');
+    } else if (key === 'c') {
+      connection.write('Say: Coming through');
+    } else if (key === 'x') {
+      connection.write('Say: i am snek');
     }
   });
 };
