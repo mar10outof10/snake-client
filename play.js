049f365a1798f8945551a24b2b9573f0d450f260
Martin Halas <echoes.time@gmail.com>
2021-03-10 01:00:49 +0000

    Added user input and exits if ctrl+c is hit

--- play.js
+++ play.js
@@ -5,5 +5,27 @@ const net = require('net');
   */
 const { connect } = require('./client')
 
+const handleUserInput = () => {
+  process.stdin.on('data', key => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+}
+
+// Setup user interface
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  handleUserInput();
+
+  return stdin;
+};
+
 console.log('Connecting ...');
 connect();
+setupInput();
+
